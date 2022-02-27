937c59d5dafbbe1167fd4b3f41eca93481e420de
Ibaihima <ibaihima@hotmail.com>
2022-02-27 12:14:11 -0500

    commit tree e4930d5594f720ea63ccc11f9ffbd6afa8c6880f

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
-  return (
-    <li className="">
+
+const [inCart, notInCart] = useState(false);
+
+function changeCart() {
+  notInCart((inCart) => !inCart);
+}
+
+const classChange = inCart ? "in-cart" : "";
+  return ( 
+    <li className={classChange}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={changeCart} className="add">{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
 
 export default Item;
+
